15d67f0ab42b7f51c97edfc106e69efe7ed72c65
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-30 14:51:40 +1000

    lab complete - added 'let likeHeartI' to for loop for closure so that the manipulation happens to the item clicked and not the last item in the loop. Adjusted the style so that the cursor is a pointer when the heart is full. Added the syntax to remove the likes

--- main.js
+++ main.js
@@ -4,23 +4,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
 let likeHeart = document.getElementsByClassName('like-glyph')
 for (i=0; i<likeHeart.length; i++){
-  likeHeartI = likeHeart[i]
+  let likeHeartI = likeHeart[i]
   likeHeartI.addEventListener('click', function(){
     mimicServerCall()
     .then(resolve => {
+      if (likeHeartI.innerHTML === EMPTY_HEART){
       alert("You liked this post!")
       console.log(resolve)
       console.log(likeHeartI.innerHTML = FULL_HEART)
       likeHeartI.setAttribute('class', 'activated-heart')
-    })
+      likeHeartI.setAttribute('style', "cursor: pointer")
+    } else if (likeHeartI.innerHTML === FULL_HEART){
+      likeHeartI.removeAttribute('class', 'activated-heart')
+      likeHeartI.innerHTML = EMPTY_HEART
+    }
+  })
     .catch((reject) => {
       let modal = document.getElementById('modal')
       modal.removeAttribute('class', 'hidden')
       document.getElementById('modal-message').innerHTML = reject
-      alert("Random server error!")
       console.log(reject)
       setTimeout(function(){
         modal.setAttribute('class', 'hidden')
@@ -29,9 +33,6 @@ for (i=0; i<likeHeart.length; i++){
 
 })
 }
-
-
-
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
